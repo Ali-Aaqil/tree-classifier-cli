5325e93fea296e31ab3437c8891e8634c0a8409f
Ali-Aaqil <aaqil.ali19@gmail.com>
2025-04-24 15:57:02 +1200

    Edited README with instructions & added image classifier to image_testing file. Changed file output location.

--- image_testing.py
+++ image_testing.py
@@ -1,6 +1,20 @@
 from pathlib import Path
+from imageai.Classification import ImageClassification
+import os
 
-def list_images(folder_path):
+def image_analysis(image_file):
+    
+    execution_path = os.getcwd()
+    prediction = ImageClassification()
+    prediction.setModelTypeAsResNet50()
+    prediction.setModelPath(os.path.join(execution_path, "resnet50-19c8e357.pth"))
+    prediction.loadModel()
+
+    predictions, probabilities = prediction.classifyImage(str(image_file), result_count=5 )
+    for eachPrediction, eachProbability in zip(predictions, probabilities):
+        print(eachPrediction , " : " , eachProbability)
+
+def list_images(folder_path, output_path):
     # Convert to Path object
     folder = Path(folder_path)
 
@@ -9,6 +23,7 @@ def list_images(folder_path):
         if image_file.suffix.lower() in [".jpg", ".jpeg", ".png"]:
             print(image_file.name)
             #todo apply image scanning technique, recieve output, right now we just return a debug object
+            analysis = image_analysis(image_file)
             scan_result = {
                 'file_name': image_file.name,
                 'tree_status': 'Yes'
@@ -17,16 +32,14 @@ def list_images(folder_path):
             print(scan_result)
             
             
-            write_results(scan_result['file_name'], scan_result['tree_status'])
-
-
+            write_results(scan_result['file_name'], scan_result['tree_status'], output_path)
 
 # todo
 # Given a scanned image output, i want to update an output file (txt file) with the image file name and whether there is a tree in said image
 
-def write_results(file_name, tree_status):
+def write_results(file_name, tree_status, output_path):
     #todo remove static write location
-    output_file = "C:\\scratch\\outputs\\output.txt"
+    output_file = f"{output_path}\\output.txt"
     output_path = Path(output_file)
 
     filename = file_name
@@ -39,4 +52,5 @@ def write_results(file_name, tree_status):
 if __name__ == "__main__":
     #write_results("test1", "Yes")
     folder_path = input("Enter the path to the folder: ").strip()
-    list_images(folder_path)
+    output_path = input("Enter an output folder path: ").strip()
+    list_images(folder_path, output_path)
